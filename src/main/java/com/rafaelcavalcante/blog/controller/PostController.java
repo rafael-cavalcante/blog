527c17db20069fa041e603b389ee4d6ff9cac6b4
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2025-03-15 10:19:21 -0300

    Add post detail view and enhance post listing with Bootstrap styling

--- src/main/java/com/rafaelcavalcante/blog/controller/PostController.java
+++ src/main/java/com/rafaelcavalcante/blog/controller/PostController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.rafaelcavalcante.blog.model.Post;
 import com.rafaelcavalcante.blog.service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PostController {
@@ -16,7 +18,15 @@ public class PostController {
     @GetMapping("/posts")
     public ModelAndView listarPots() {
         ModelAndView modelAndView = new ModelAndView("posts");
-        modelAndView.addObject("posts", postService.findAll());
+        modelAndView.addObject("posts", this.postService.findAll());
+        return modelAndView;
+    }
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView buscarPost(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("visualizarPost");
+        Post post = this.postService.findById(id);
+        modelAndView.addObject("post", post);
         return modelAndView;
     }
 }
