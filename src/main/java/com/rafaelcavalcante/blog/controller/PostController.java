0fdca5500e937c225987f478c2063f77a06a83ac
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2025-03-15 11:00:18 -0300

    Add post creation form and enhance navigation in blog templates

--- src/main/java/com/rafaelcavalcante/blog/controller/PostController.java
+++ src/main/java/com/rafaelcavalcante/blog/controller/PostController.java
@@ -1,13 +1,21 @@
 package com.rafaelcavalcante.blog.controller;
 
+import java.time.LocalDate;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rafaelcavalcante.blog.model.Post;
 import com.rafaelcavalcante.blog.service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class PostController {
@@ -29,4 +37,23 @@ public class PostController {
         modelAndView.addObject("post", post);
         return modelAndView;
     }
+
+    @GetMapping("/posts/newpost")
+    public ModelAndView getPostForm() {
+        ModelAndView modelAndView = new ModelAndView("postForm");
+        modelAndView.addObject("post", new Post());
+        return modelAndView;
+    }
+
+    @PostMapping("/posts/newpost")
+    public String savePostForm(@Valid Post post, BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/posts/newPost";
+        }
+        post.setDate(LocalDate.now());
+        this.postService.save(post);
+        return "redirect:/posts";
+    }
+
 }
